2cd120bd5974ed93fe0ed1b374713a8726f46a95
LuckyGStar <andylee.dreamsoft@gmail.com>
2024-09-06 01:47:09 -0400

    to-do list 🙈

---
+++ main.ts
@@ -0,0 +1,11 @@
+///////////////////////
+//Anatta Coding Challenge
+///////////////////////
+
+/*
+
+Project Brief:
+
+"Create a script(eg: app.js) that communicates with Shopify through Shopify's graphql APIs. It takes input product names and fetches the 
+appropriate products that match the name and list down the variants sorting by price."
+*/
